626c229d48023d919288872dc3b790cd140d52fe
Chetan Ramesh Bendkule <chetanbendkule0707@gmail.com>
2025-04-02 00:25:02 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT ="http://localhost:8080/api/v1/user";
+export const API_END_POINT ="https://netflix-backend-1-1pr5.onrender.com/api/v1/user";
 export const Now_Playing_Movie ="https://api.themoviedb.org/3/movie/now_playing";
 export const Popular_Movie ="https://api.themoviedb.org/3/movie/popular";
 export const Top_Rated_Movie ="https://api.themoviedb.org/3/movie/top_rated";
